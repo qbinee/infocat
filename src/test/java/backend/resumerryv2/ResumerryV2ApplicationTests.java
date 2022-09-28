8541765805d8485c25d564e08ad87fb2fd36c25e
qbinee <sdhg12@naver.com>
2022-09-28 20:11:38 +0900

    feat: init project

---
+++ src/test/java/backend/resumerryv2/ResumerryV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.resumerryv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumerryV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
