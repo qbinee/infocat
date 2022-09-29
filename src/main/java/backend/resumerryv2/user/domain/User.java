e094a67ddaee750b2537d483384a19e2dc7e92c4
qbinee <sdhg12@naver.com>
2022-09-29 12:27:51 +0900

    feat: base entity 와 user entity 생성

--- src/main/java/backend/resumerryv2/user/domain/User.java
+++ src/main/java/backend/resumerryv2/user/domain/User.java
@@ -1,13 +1,50 @@
-package backend.resumerryv2.user.domain.repository;
+package backend.resumerryv2.user.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @NoArgsConstructor
 @Entity
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String nickname;
+
+    @Nullable
+    private Integer years;
+
+    @Nullable
+    private Enum<UserCategory> category;
+
+    @Nullable
+    private String introduce;
+
+    @Nullable
+    private String imageSrc;
+
+    @Builder
+    public User(Long id, String email, String nickname, Integer years, Enum<UserCategory> category,
+                String introduce, String imageSrc) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+        this.years = years;
+        this.category = category;
+        this.introduce = introduce;
+        this.imageSrc = imageSrc;
+    }
+
 }
+
