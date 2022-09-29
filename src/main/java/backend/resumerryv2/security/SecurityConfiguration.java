917ae43adf0b8049decd6d889030b8020defe5d2
qbinee <sdhg12@naver.com>
2022-09-29 12:26:58 +0900

    feat: spring security 초기 설정

--- src/main/java/backend/resumerryv2/security/SecurityConfiguration.java
+++ src/main/java/backend/resumerryv2/security/SecurityConfiguration.java
@@ -1,2 +1,31 @@
-package backend.resumerryv2.security;public class SecurityConfiguration {
+package backend.resumerryv2.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+
+                .httpBasic()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/auth/**, /auth, /swagger-ui.html")
+                .permitAll();
+        return http.build();
+    }
+
+
 }
