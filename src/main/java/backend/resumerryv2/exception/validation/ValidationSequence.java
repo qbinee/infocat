251b9359f2cd281844fc81793bccdfb29adc52eb
qbinee <sdhg12@naver.com>
2022-10-04 00:35:48 +0900

    feat: validation error format 추가

---
+++ src/main/java/backend/resumerryv2/exception/validation/ValidationSequence.java
@@ -0,0 +1,8 @@
+package backend.resumerryv2.exception.validation;
+
+import javax.validation.GroupSequence;
+import javax.validation.groups.Default;
+
+@GroupSequence({Default.class, ValidationGroups.NotEmptyGroup.class, ValidationGroups.PatternCheckGroup.class,})
+public interface ValidationSequence {
+}
