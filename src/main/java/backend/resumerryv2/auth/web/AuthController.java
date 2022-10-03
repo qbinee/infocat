70589ccce8d8a2178dc1a49cf7ccd1b9ba1e1dec
qbinee <sdhg12@naver.com>
2022-10-03 16:00:56 +0900

    feat: 인증코드 전송 로직 설정

--- src/main/java/backend/resumerryv2/auth/web/AuthController.java
+++ src/main/java/backend/resumerryv2/auth/web/AuthController.java
@@ -1,2 +1,39 @@
-package backend.resumerryv2.auth.web;public class AuthController {
+package backend.resumerryv2.auth.web;
+
+import backend.resumerryv2.auth.domain.dto.*;
+import backend.resumerryv2.auth.service.AuthService;
+import backend.resumerryv2.auth.service.EmailService;
+import backend.resumerryv2.global.dto.GlobalResponse;
+import com.sun.mail.smtp.SMTPSendFailedException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+    private final EmailService emailService;
+
+    @PostMapping("/validation")
+    public ResponseEntity<GlobalResponse> checkSignUpValidation(
+            @RequestBody ValidationRequest validationRequest
+            ){
+        return ResponseEntity.ok(GlobalResponse.ofSuccess());
+    }
+
+    @PostMapping("/email")
+    public ResponseEntity<GlobalResponse> sendValidationCodeEmail(
+            @RequestBody EmailRequest email
+    ){
+        SimpleMailMessage simpleMailMessage = emailService.setValidationCodeEmailForm(email.getEmail());
+        emailService.sendEmail(email.getEmail(), simpleMailMessage);
+        return ResponseEntity.ok(GlobalResponse.ofSuccess());
+    }
 }
---
+++ src/main/java/backend/resumerryv2/security/config/WebConfiguration.java
@@ -0,0 +1,2 @@
+package backend.resumerryv2.security.config;public class WebConfiguration {
+}
