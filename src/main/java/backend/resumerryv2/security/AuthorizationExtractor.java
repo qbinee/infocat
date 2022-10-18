066d3fd48b17319b4d878d7a1b59461883fed1f8
qbinee <sdhg12@naver.com>
2022-10-18 20:51:01 +0900

    feat: Cookie를 이용한 인증로직 변경

--- src/main/java/backend/resumerryv2/security/AuthorizationExtractor.java
+++ src/main/java/backend/resumerryv2/security/AuthorizationExtractor.java
@@ -6,15 +6,15 @@ import java.util.Objects;
 public class AuthorizationExtractor {
 
     private static final String COOKIE = "Cookie";
-    private static final String BEARER_TYPE = "Bearer";
+    private static final String ACCESSTOKEN_TYPE = "AccessToken=";
 
     private AuthorizationExtractor() {}
 
     public static String extract(HttpServletRequest request) {
         String authorization = request.getHeader(COOKIE);
         if (!Objects.isNull(authorization)
-                && authorization.toLowerCase().startsWith(BEARER_TYPE.toLowerCase())) {
-            String tokenValue = authorization.substring(BEARER_TYPE.length()).trim();
+                && authorization.toLowerCase().startsWith(ACCESSTOKEN_TYPE.toLowerCase())) {
+            String tokenValue = authorization.substring(ACCESSTOKEN_TYPE.length()).trim();
             int commaIndex = tokenValue.indexOf(',');
             if (commaIndex > 0) {
                 tokenValue = tokenValue.substring(0, commaIndex);
