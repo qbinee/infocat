2d33c1c3a40d9b2d178e3536fe6a7f780c9017fe
qbinee <sdhg12@naver.com>
2022-10-18 21:56:47 +0900

    refactor: cors 설정 및 cookie 설정 변경

--- src/main/java/backend/resumerryv2/security/config/SecurityConfiguration.java
+++ src/main/java/backend/resumerryv2/security/config/SecurityConfiguration.java
@@ -30,6 +30,8 @@ public class SecurityConfiguration {
 
                 .httpBasic()
                 .and()
+                .cors()
+                .and()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
